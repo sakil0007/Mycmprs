c508356834fdba7b5751533771f1c26ee8fd6d54
Sakil0007 <itsmesakil.tech@gmail.com>
2023-05-12 20:21:04 +0530

    Add files via upload

--- helper/funcn.py
+++ helper/funcn.py
@@ -18,7 +18,7 @@ COUNT = []
 OK = {}
 
 uptime = dt.now()
-os.system("wget https://telegra.ph/file/75ee20ec8d8c8bba84f02.jpg -O thumb.jpg")
+os.system("wget https://telegra.ph/file/c8510dc787aa503705ea1.jpg -O thumb.jpg")
 
 if not os.path.isdir("downloads/"):
     os.mkdir("downloads/")
--- helper/stuff.py
+++ helper/stuff.py
@@ -33,8 +33,8 @@ async def start(event):
         buttons=[
             [Button.inline("HELP", data="ihelp")],
             [
-                Button.url("SOURCE CODE", url="github.com/1Danish-00/CompressorBot"),
-                Button.url("DEVELOPER", url="t.me/danish_00"),
+                Button.url("SOURCE CODE", url="t.me/Its_Me_SakiL"),
+                Button.url("DEVELOPER", url="t.me/its_Me_SakiL"),
             ],
         ],
     )
@@ -60,8 +60,8 @@ async def beck(event):
         buttons=[
             [Button.inline("HELP", data="ihelp")],
             [
-                Button.url("SOURCE CODE", url="github.com/1Danish-00/"),
-                Button.url("DEVELOPER", url="t.me/danish_00"),
+                Button.url("SOURCE CODE", url="t.me/Its_Me_SakiL"),
+                Button.url("DEVELOPER", url="t.me/Its_Me_SakiL"),
             ],
         ],
     )
